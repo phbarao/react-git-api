e13ef78c848b8695cbd6f61632b9e1014c22323b
Pedro Barão <phbarao@gmail.com>
2021-03-30 19:19:21 -0300

    creating app, routes and global styles

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { Title } from './styles';
+
+// Usando const é mais fácil tipar o componente
+const Dashboard: React.FC = () => {
+  return <Title>Explore repositórios no Github</Title>;
+};
+
+export default Dashboard;
---
+++ src/pages/Repository/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// Usando const é mais fácil tipar o componente
+const Repository: React.FC = () => {
+  return <h1>Repository</h1>;
+};
+
+export default Repository;
