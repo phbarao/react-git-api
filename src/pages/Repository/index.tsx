e3455684040a6a5fa24a2fa4acf02830bab59cf6
Pedro Barão <phbarao@gmail.com>
2021-04-04 14:00:00 -0300

    routes navigation

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import { useRouteMatch } from 'react-router-dom';
+
+interface RepositoryParams {
+  repository: string;
+}
 
 // Usando const é mais fácil tipar o componente
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>;
+  const { params } = useRouteMatch<RepositoryParams>();
+
+  return <h1>Repository:{params.repository}</h1>;
 };
 
 export default Repository;
